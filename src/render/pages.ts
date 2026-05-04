e9f82847aa25c6ef939633a6d7c687c15c09b4be
studio <studio@studioui-MacBookPro.local>
2026-05-04 11:54:48 +0900

    Stabilize home lead image frame

--- src/render/pages.ts
+++ src/render/pages.ts
@@ -276,7 +276,7 @@ ${imageGrid}
         </figure>`;
 };
 
-const assetVersion = "20260504-home-rhythm-highlight";
+const assetVersion = "20260504-home-image-aperture";
 
 const renderLanguageSwitch = (currentPath: string, locale: Locale): string => `
   <div class="language-switch" aria-label="Language switcher">
