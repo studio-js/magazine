8eb2e86dd1c694cc1551c0ddf29ad95e0ec06c17
studio <studio@studioui-MacBookPro.local>
2026-05-04 16:47:20 +0900

    Refine Korean typography restraint

--- src/render/pages.ts
+++ src/render/pages.ts
@@ -276,7 +276,7 @@ ${imageGrid}
         </figure>`;
 };
 
-const assetVersion = "20260504-pretendard-type";
+const assetVersion = "20260504-diagram-restraint";
 
 const renderLanguageSwitch = (currentPath: string, locale: Locale): string => `
   <div class="language-switch" aria-label="Language switcher">
